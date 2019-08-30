5291c56c6ff0cd8a949e130eb06783e0e09b1077
psyche-ps <ping_ps@163.com>
2019-08-30 14:23:43 +0800

    1

--- tutorials/02-intermediate/language_model/main.py
+++ tutorials/02-intermediate/language_model/main.py
@@ -8,14 +8,14 @@ from data_utils import Dictionary, Corpus
 
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters
 embed_size = 128
 hidden_size = 1024
 num_layers = 1
-num_epochs = 5
-num_samples = 1000     # number of words to be sampled
+num_epochs = 2
+num_samples = 100     # number of words to be sampled
 batch_size = 20
 seq_length = 30
 learning_rate = 0.002
